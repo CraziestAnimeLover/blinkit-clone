feb6491d5a13f18ab0eb99ad2bc696cf49fffa24
CraziestAnimeLover <riyarsunny706@gmail.com>
2025-12-25 15:47:06 +0530

    add tailwind css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-    
-  ],base: "/"
+  plugins: [react()],
+  build: {
+    outDir: 'build' // Vercel expects 'build'
+  },
+  base: '/' // ensures correct routing
 })
